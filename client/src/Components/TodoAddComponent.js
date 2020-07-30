273597bd0b35f180123aefb9bfaf79910f2f2e91
unknown <wotjq5113@gmail.com>
2020-07-30 23:02:51 +0900

    Add TodoList Insert

--- client/src/Components/TodoAddComponent.js
+++ client/src/Components/TodoAddComponent.js
@@ -1,13 +1,65 @@
 import React from 'react';
+import {post} from 'axios';
+const tag = "[TodoAddComponent]";
 
 class TodoAddComponent extends React.Component {
     constructor(props){
         super(props);
+        console.log(tag);
+        this.state = {
+            job : '',
+            startDate : '',
+            endDate : '',
+            desc : ''
+        }
+    }
+    addTodoList = () => {
+        const url = '/api/todolists';
+        const formData = new FormData();
+        formData.append('job', this.state.job);
+        formData.append('startDate', this.state.startDate);
+        formData.append('endDate', this.state.endDate);
+        formData.append('desc', this.state.desc);
+        console.log(tag, "addTodoList()");
+
+        const config = {
+            // headers : {'content-type' : 'multipart/form-data'}
+            headers : {'content-type' : 'application/x-www-form-urlencoded'}
+        }
+        return post(url, formData, config);
+    }
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(tag,'handleFormSubmit');
+
+        this.addTodoList()
+            .then((response) => {
+                console.log('response : ' + response.data)
+            })
+
+
+        if(this.state.job !== '' && this.state.startDate !== '' && this.state.endDate !== '' && this.state.desc !== ''){
+        
+        }
+        //window.location.reload();
+
+    }
+    handleValueChange = (e) => {
+        console.log(tag,'handleValueChange');
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState)
     }
     render(){
         return(
             <form>
-                <input type = "text" placeholder="할일을 입력하세요" autoFocus></input>
+                <p>JOB : <input type = "text" placeholder="할일" name = "job" autoFocus onChange={this.handleValueChange}></input></p>
+                <p>START_DATE : <input type = "date" name = "startDate" onChange={this.handleValueChange}></input></p>
+                <p>END_DATE : <input type = "date" name = "endDate" onChange={this.handleValueChange}></input></p>
+                <p>DESC : <input type = "text"  name = "desc" onChange={this.handleValueChange}></input></p>
+                
+                
+                <input type = "submit" value = "Add"  onClick = {this.handleFormSubmit}></input>
             </form>
         )
     }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const mysql = require('mysql');
 const fs = require('fs');
+const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { POINT_CONVERSION_HYBRID } = require('constants');
 
 const app = express();
 const port = process.env.PORT || 5000;
-const data = fs.readFileSync('./database.json');
+const data = fs.readFileSync('./database.json')
 const conf = JSON.parse(data);
 const connection = mysql.createConnection({
     host : conf.host,
@@ -15,6 +17,13 @@ const connection = mysql.createConnection({
 })
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest : './upload'});
+  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+
 app.get('/api/todolists',(req,res) => {
     connection.query(
         "SELECT * FROM TODO_LIST",
@@ -24,4 +33,30 @@ app.get('/api/todolists',(req,res) => {
 
 app.listen(port, () => {
     console.log(`Listening port ${port}`);
+})
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+    } else {
+    next();
+    }
+};
+    
+app.post('/api/todolists', upload.single('image'), (req,res) => {
+
+    let sql = 'INSERT INTO TODO_LIST VALUES (NULL,?,?,?,?)';
+    
+    let id = req.body.ID;
+    console.log(req.body);
+    let params = [req.body.job,req.body.startDate,req.body.endDate,req.body.desc];
+    connection.query(sql, params,
+        (err,rows,fields) => {
+            res.send(rows);
+        }
+    )
 })
